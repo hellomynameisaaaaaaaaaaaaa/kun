ab2d2e09eeed19baa5f9ecbe7f1099825548a435
JAY APPS <jasonswen318@gmail.com>
2022-06-05 15:05:55 -0700

    more changes to lexer

--- src/parser.rs
+++ src/parser.rs
@@ -30,10 +30,14 @@ pub fn parser(text: String) {
     let mut parsed: Vec<Sentences> = vec![];
     for i in lexer_group {
         parsed.push(match i {
-            vec![Token::Word(x), Token::Assign, Token::Number(y)] => Sentences::Define(x, Types::Number, Value::Number(y)), // x = y; where y is a valid number
+            // ----- define variable -----
+            // -- inferred type --
+            vec![Token::Word(x), Token::Assign,  Token::Number(y)] => Sentences::Define(x, Types::Number, Value::Number(y)), // x = y; where y is a valid number
+            vec![Token::Word(x), Token::Assign,  Token::String(y)] => Sentences::Define(x, Types::String, Value::String(y)), // x = y; where y is a valid string
+            // -- declared type --
             vec![Token::Word(x), Token::SetType, Token::Type(Types::Number), Token::Number(y)] => Sentences::Define(x, Types::Number, Value::Number(y)), // x: number = y;
-            vec![Token::Word(x), Token::Assign, Token::String(y)] => Sentences::Define(x, Types::String, Value::String(y)), // x = y; where y is a valid string
             vec![Token::Word(x), Token::SetType, Token::Type(Types::String), Token::String(y)] => Sentences::Define(x, Types::String, Value::String(y)), // x: string = y;
+
             _ => Sentences::Error
         })
 
